4cdf12276a4087e22ed4690c64099ec64b095149
mogyorosimartin <43951510+mogyorosimartin@users.noreply.github.com>
2019-01-14 16:33:39 +0100

    Update csvlog.py

--- csvlog.py
+++ csvlog.py
@@ -17,8 +17,9 @@ def csvlog(h,t,dewpoint,date,csvname,cam):
 				#os.system('fswebcam -r 1280x720 {:%Y_%m_%d__%H_%M}.jpg >/dev/null'.format(date))
 				with open(os.devnull, 'wb') as devnull:
 					subprocess.check_call(['fswebcam', '-r', '1280x720','-S','21','--timestamp','%Y-%m-%d %H:%M','--banner-colour','#FF000000','--line-colour','#FF000000','--title','Hőmérséklet:{:s}°C     Páratartalom:{:s}%     Harmatpont:{:s}°C'.format(t,h,dewpoint),'/home/pi/Weather/Images/{:%Y_%m_%d__%H_%M}.png'.format(date)], stdout=devnull, stderr=subprocess.STDOUT)
+				ftpupload.ftpupload('{:%Y_%m_%d__%H_%M}.png'.format(date))
 		#print('{:%Y_%m_%d__%H_%M}.png'.format(date))
-		ftpupload.ftpupload('{:%Y_%m_%d__%H_%M}.png'.format(date))	
+			
 		
 		print('CSV:    OK')			
 	except:
